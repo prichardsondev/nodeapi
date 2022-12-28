81a671c713856333d05407f7b912f147965e8ce6
prichardsondev <prichardsondev@gmail.com>
2022-12-28 08:33:50 -0500

    added note for get coords button

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,10 @@ function App() {
 
   return (
     <main>
+      <p>To use Get Coordinates button</p>
+      <p>Mac Users 'System Preferences > Security & Privacy > Privacy > Location Services'</p>
+      <p>All Users must accept the option for the browser to use your location... </p>
+      <p>Or just paste in your coords to avoid using the button</p>
       <Weather />
     </main>
   );
